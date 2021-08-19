33afa1f1ef7308a153caddf1f4ad63b17ed5a63a
jatinjd17 <jatindharmik@gmail.com>
2021-08-19 17:11:22 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,182 @@
+var aspire = require("./Database1/Ford/Aspire/AllSpecs.json");
+var ecosport = require("./Database1/Ford/Ecosport/AllSpecs.json");
+var endeavour = require("./Database1/Ford/Endeavour/AllSpecs.json");
+var figo = require("./Database1/Ford/Figo/AllSpecs.json");
+var freestyle = require("./Database1/Ford/Freestyle/AllSpecs.json");
+var amaze = require("./Database1/Honda/Amaze/AllSpecs.json");
+var city = require("./Database1/Honda/City/AllSpecs.json");
+var city4 = require("./Database1/Honda/City4/AllSpecs.json");
+var jazz = require("./Database1/Honda/Jazz/AllSpecs.json");
+var wrv = require("./Database1/Honda/Wrv/AllSpecs.json");
+var alcazar = require("./Database1/Hyundai/Alcazar/AllSpecs.json");
+var aura = require("./Database1/Hyundai/Aura/AllSpecs.json");
+var creta = require("./Database1/Hyundai/Creta/AllSpecs.json");
+var elantra = require("./Database1/Hyundai/Elantra/AllSpecs.json");
+var i10 = require("./Database1/Hyundai/i10/AllSpecs.json");
+var i20 = require("./Database1/Hyundai/i20/AllSpecs.json");
+var konaelectric = require("./Database1/Hyundai/KonaElectric/AllSpecs.json");
+var santro = require("./Database1/Hyundai/Santro/AllSpecs.json");
+var tucson = require("./Database1/Hyundai/Tucson/AllSpecs.json");
+var venue = require("./Database1/Hyundai/Venue/AllSpecs.json");
+var verna = require("./Database1/Hyundai/Verna/AllSpecs.json");
+var xcent = require("./Database1/Hyundai/Xcent/AllSpecs.json");
+var carnival = require("./Database1/Kia/Carnival/AllSpecs.json");
+var seltos = require("./Database1/Kia/Seltos/AllSpecs.json");
+var sonet = require("./Database1/Kia/Sonet/AllSpecs.json");
+var alturas = require("./Database1/Mahindra/Alturas/AllSpecs.json");
+var bolero = require("./Database1/Mahindra/Bolero/AllSpecs.json");
+var boleroneo = require("./Database1/Mahindra/BoleroNeo/AllSpecs.json");
+var boleropickup = require("./Database1/Mahindra/BoleroPickup/AllSpecs.json");
+var kuv100 = require("./Database1/Mahindra/Kuv100/AllSpecs.json");
+var marazzo = require("./Database1/Mahindra/Marazzo/AllSpecs.json");
+var scorpio = require("./Database1/Mahindra/Scorpio/AllSpecs.json");
+var thar = require("./Database1/Mahindra/Thar/AllSpecs.json");
+var veritoev = require("./Database1/Mahindra/VeritoEV/AllSpecs.json");
+var xuv300 = require("./Database1/Mahindra/XUV300/AllSpecs.json");
+var xuv500 = require("./Database1/Mahindra/XUV500/AllSpecs.json");
+var alto = require("./Database1/MarutiSuzuki/Alto/AllSpecs.json");
+var baleno = require("./Database1/MarutiSuzuki/Baleno/AllSpecs.json");
+var brezza = require("./Database1/MarutiSuzuki/Brezza/AllSpecs.json");
+var celerio = require("./Database1/MarutiSuzuki/Celerio/AllSpecs.json");
+var celeriox = require("./Database1/MarutiSuzuki/CelerioX/AllSpecs.json");
+var ciaz = require("./Database1/MarutiSuzuki/Ciaz/AllSpecs.json");
+var dzire = require("./Database1/MarutiSuzuki/Dzire/AllSpecs.json");
+var dziretour = require("./Database1/MarutiSuzuki/DzireTour/AllSpecs.json");
+var eco = require("./Database1/MarutiSuzuki/Eco/AllSpecs.json");
+var ertiga = require("./Database1/MarutiSuzuki/Ertiga/AllSpecs.json");
+var ignis = require("./Database1/MarutiSuzuki/Ignis/AllSpecs.json");
+var scross = require("./Database1/MarutiSuzuki/Scross/AllSpecs.json");
+var spresso = require("./Database1/MarutiSuzuki/Spresso/AllSpecs.json");
+var swift = require("./Database1/MarutiSuzuki/Swift/AllSpecs.json");
+var waganr = require("./Database1/MarutiSuzuki/WaganR/AllSpecs.json");
+var xl6 = require("./Database1/MarutiSuzuki/XL6/AllSpecs.json");
+var gloster = require("./Database1/MG/Gloster/AllSpecs.json");
+var hector = require("./Database1/MG/Hector/AllSpecs.json");
+var hectorplus = require("./Database1/MG/HectorPlus/AllSpecs.json");
+var zsev = require("./Database1/MG/ZSEV/AllSpecs.json");
+var gtr = require("./Database1/Nissan/GTR/AllSpecs.json");
+var kicks = require("./Database1/Nissan/Kicks/AllSpecs.json");
+var magnite = require("./Database1/Nissan/Magnite/AllSpecs.json");
+var duster = require("./Database1/Renault/Duster/AllSpecs.json");
+var kiger = require("./Database1/Renault/Kiger/AllSpecs.json");
+var kwid = require("./Database1/Renault/Kwid/AllSpecs.json");
+var triber = require("./Database1/Renault/Triber/AllSpecs.json");
+var kushaq = require("./Database1/Skoda/Kushaq/AllSpecs.json");
+var octavia = require("./Database1/Skoda/Octavia/AllSpecs.json");
+var rapid = require("./Database1/Skoda/Rapid/AllSpecs.json");
+var superb = require("./Database1/Skoda/Superb/AllSpecs.json");
+var altroz = require("./Database1/Tata/Altroz/AllSpecs.json");
+var harrier = require("./Database1/Tata/Harrier/AllSpecs.json");
+var nexon = require("./Database1/Tata/Nexon/AllSpecs.json");
+var nexonev = require("./Database1/Tata/NexonEV/AllSpecs.json");
+var safari = require("./Database1/Tata/Safari/AllSpecs.json");
+var tiago = require("./Database1/Tata/Tiago/AllSpecs.json");
+var tigor = require("./Database1/Tata/Tigor/AllSpecs.json");
+var tigorev = require("./Database1/Tata/TigorEV/AllSpecs.json");
+var yodha = require("./Database1/Tata/Yodha/AllSpecs.json");
+var camry = require("./Database1/Toyota/Camry/AllSpecs.json");
+var fortuner = require("./Database1/Toyota/Fortuner/AllSpecs.json");
+var glanza = require("./Database1/Toyota/Glanza/AllSpecs.json");
+var innova = require("./Database1/Toyota/Innova/AllSpecs.json");
+var urbancruiser = require("./Database1/Toyota/UrbanCruiser/AllSpecs.json");
+var vellfire = require("./Database1/Toyota/Vellfire/AllSpecs.json");
+var yaris = require("./Database1/Toyota/Yaris/AllSpecs.json");
+var polo = require("./Database1/Volkswagen/Polo/AllSpecs.json");
+var tiguan = require("./Database1/Volkswagen/TigualAllSpace/AllSpecs.json");
+var troc = require("./Database1/Volkswagen/TRoc/AllSpecs.json");
+var vento = require("./Database1/Volkswagen/Vento/AllSpecs.json");
+// and so on
+
+module.exports = function () {
+  return {
+    ///////////Ford
+    aspire: aspire,
+    ecosport: ecosport,
+    endeavour: endeavour,
+    figo: figo,
+    freestyle: freestyle,
+    /////////Honda
+    amaze: amaze,
+    city: city,
+    city4: city4,
+    jazz: jazz,
+    wrv: wrv,
+    alcazar: alcazar,
+    aura: aura,
+    creta: creta,
+    elantra: elantra,
+    i10: i10,
+    i20: i20,
+    konaelectric: konaelectric,
+    santro: santro,
+    tucson: tucson,
+    venue: venue,
+    verna: verna,
+    xcent: xcent,
+    carnival: carnival,
+    seltos: seltos,
+    sonet: sonet,
+    alturas: alturas,
+    bolero: bolero,
+    boleroneo: boleroneo,
+    boleropickup: boleropickup,
+    kuv100: kuv100,
+    marazzo: marazzo,
+    scorpio: scorpio,
+    thar: thar,
+    veritoev: veritoev,
+    xuv300: xuv300,
+    xuv500: xuv500,
+    alto: alto,
+    baleno: baleno,
+    brezza: brezza,
+    celerio: celerio,
+    celeriox: celeriox,
+    ciaz: ciaz,
+    dzire: dzire,
+    dziretour: dziretour,
+    eco: eco,
+    ertiga: ertiga,
+    ignis: ignis,
+    scross: scross,
+    spresso: spresso,
+    swift: swift,
+    waganr: waganr,
+    xl6: xl6,
+    gloster: gloster,
+    hector: hector,
+    hectorplus: hectorplus,
+    zsev: zsev,
+    gtr: gtr,
+    kicks: kicks,
+    magnite: magnite,
+    duster: duster,
+    kiger: kiger,
+    kwid: kwid,
+    triber: triber,
+    kushaq: kushaq,
+    octavia: octavia,
+    rapid: rapid,
+    superb: superb,
+    altroz: altroz,
+    harrier: harrier,
+    nexon: nexon,
+    nexonev: nexonev,
+    safari: safari,
+    tiago: tiago,
+    tigor: tigor,
+    tigorev: tigorev,
+    yodha: yodha,
+    camry: camry,
+    fortuner: fortuner,
+    glanza: glanza,
+    innova: innova,
+    urbancruiser: urbancruiser,
+    vellfire: vellfire,
+    yaris: yaris,
+    polo: polo,
+    tiguan: tiguan,
+    troc: troc,
+    vento: vento,
+  };
+};
